1ed1d3948ae4daf226ff4cf400bc67592a1c0c6e
ismail.bpn@gmail.com <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2019-11-03 23:27:09 +0700

    nambahin connectDB

--- node_demo/index.js
+++ node_demo/index.js
@@ -3,6 +3,26 @@ const express = require('express')
 const app = express()
 var somerandomname = require('./somefunc')
 
+//connecting to pq-promise
+var pgp = require('pg-promise')
+var dbPromise = pgp('postgres://lasti@178.128.104.74')
+
+function connectDB(){
+  dbPromise.any('SELECT * FROM pegawai')
+            .then(data => {
+              return data
+            })
+            .catch(err => {
+              return err;
+            })
+}
+
+app.get('/pegawai', function (req,res) {
+  res.send(connectDB())
+})
+
+
+// below are just trying
 app.get('/', function (req,res) {
   res.send('Hello World')
 })
